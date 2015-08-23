b965cc47445751f0bf751b0cab662c4b2bd28c32
perryspy <perry.spy@gmail.com>
2015-08-23 12:08:40 -0400

    basic google maps capability

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,32 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+
+		// default location
+		var location = new google.maps.LatLng(32.7765656, -79.93092159);
+
+		// map object
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 13,
+			center: location
+		});
+
+		// find latlong
+		var geocoder = new google.maps.Geocoder();
+
+
+		$scope.updateLocation = function() {
+			geocoder.geocode({'address': $scope.searchText}, function (response) {
+				map.setCenter(response[0].geometry.location);
+			});
+		};
+
+		//var marker = new google.maps.Marker({
+		//	position: center,
+		//	map: map,
+		//	title: ''
+		//});
+
 	}
 ]);
